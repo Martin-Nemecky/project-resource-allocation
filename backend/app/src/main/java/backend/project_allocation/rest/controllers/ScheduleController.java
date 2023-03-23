3d74d668b0d2f78ffcc59f19554e577fbd304dde
Martin Německý <nemecm64@fit.cvut.cz>
2023-03-23 17:16:34 +0100

    Add endpoints to controller

--- backend/app/src/main/java/backend/project_allocation/rest/controllers/ScheduleController.java
+++ backend/app/src/main/java/backend/project_allocation/rest/controllers/ScheduleController.java
@@ -1,4 +1,4 @@
-package backend.project_allocation.rest;
+package backend.project_allocation.rest.controllers;
 
 import backend.project_allocation.domain.Schedule;
 import backend.project_allocation.rest.converters.ScheduleConverter;
@@ -6,31 +6,42 @@ import backend.project_allocation.rest.dtos.ScheduleDto;
 import backend.project_allocation.services.ScheduleService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/schedule")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    public ScheduleController(ScheduleService scheduleService) {
+    private final ScheduleConverter scheduleConverter;
+
+    public ScheduleController(ScheduleService scheduleService, ScheduleConverter scheduleConverter) {
         this.scheduleService = scheduleService;
+        this.scheduleConverter = scheduleConverter;
     }
 
     @PostMapping
     public ScheduleDto solve(@RequestBody ScheduleDto scheduleDto) {
-       scheduleService.solveLive(ScheduleConverter.fromDto(scheduleDto));
-       return scheduleDto;
+       Schedule schedule = scheduleConverter.fromDto(scheduleDto);
+       scheduleService.solveLive(schedule, (long) scheduleDto.getConfigurationParameters().getTerminationTimeInMinutes());
+       return scheduleConverter.toDto(schedule);
     }
 
-//    @GetMapping("/best")
-//    public ScheduleDto getBest() {
-//        Schedule bestSchedule = scheduleService.findBestSolution();
-//        return scheduleConverter.toDto(bestSchedule);
-//    }
+    @GetMapping("/best")
+    public ScheduleDto getBest() {
+        Schedule bestSchedule = scheduleService.findBestSolution();
+        return scheduleConverter.toDto(bestSchedule);
+    }
 
-//    @GetMapping
-//    public List<ScheduleDto> getAll() {
-//        List<Schedule> schedules = scheduleService.findAll();
-//        return scheduleConverter.toDtoList(schedules);
-//    }
+    @GetMapping
+    public List<ScheduleDto> getAll() {
+        List<Schedule> schedules = scheduleService.findAll();
+        return scheduleConverter.toDtoList(schedules);
+    }
+
+    @PutMapping("/{id}")
+    public void stopSolving(@PathVariable Long id){
+        scheduleService.stopSolving(id);
+    }
 }
