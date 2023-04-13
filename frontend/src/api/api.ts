e7a8fde00e78744c0fa4ece95d84bd2369ca7be4
Martin Německý <nemecm64@fit.cvut.cz>
2023-04-13 10:57:18 +0200

    Add functions for sending and retrieving data from backend

---
+++ frontend/src/api/api.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { PreviewDto, ScheduleDto } from "../data/dtoTypes";
+
+const url = "http://localhost:8080/schedule";
+
+export function solve(schedule : ScheduleDto) {
+    return axios.post<PreviewDto>(url, schedule);
+}
+
+export function stopSolving(id : number | undefined) {
+    if(id === undefined){
+        return Promise.reject(new Error("Cannot stop solving with undefined id."))
+    }
+
+    return axios.put(url + "/" + id);
+}
+
+export function getLastScheduleVersion(id : number) {
+    return axios.get<ScheduleDto>(url + "/" + id + "/versions/last");
+}
+
+export function getAllScheduleVersions(id : number) {
+    return axios.get<ScheduleDto[]>(url + "/" + id + "/versions");
+}
+
+export function getScheduleByVersion(id : number, version : number) {
+    return axios.get<ScheduleDto>(url + "/" + id + "/versions/" + version);
+}
+
+export function getLastPreview(id : number) {
+    return axios.get<PreviewDto>(url + "/" + id + "/previews/last");
+}
+
+export function getPreview(id : number, versionId : number) {
+    return axios.get<PreviewDto>(url + "/" + id + "/previews/" + versionId);
+}
