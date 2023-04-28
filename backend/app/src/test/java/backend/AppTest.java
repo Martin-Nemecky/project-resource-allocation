a36def3ad528e1720b989069aaad4fffb64bf328
Martin Německý <nemecm64@fit.cvut.cz>
2023-04-28 09:00:46 +0200

    Add empty AppTest.java

--- backend/app/src/test/java/backend/AppTest.java
+++ backend/app/src/test/java/backend/AppTest.java
@@ -3,12 +3,9 @@
  */
 package backend;
 
+import backend.project_allocation.App;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+
 }
